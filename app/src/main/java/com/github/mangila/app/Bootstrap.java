afec1e67506eb8c778dd479c9793d6aca670d896
Erik Olsson <olsson.erik1993@gmail.com>
2025-10-29 13:35:57 +0100

    bootstrap class

--- app/src/main/java/com/github/mangila/app/Bootstrap.java
+++ app/src/main/java/com/github/mangila/app/Bootstrap.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Component;
 
 /**
  * Simple bootstrap class to load some fake data on start-up.
+ * <br>
+ * This is how to load some fake data, since our application is responsible for the business logic.
  */
 @Component
 @Profile("dev")
