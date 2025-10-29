a6883876f07ebc83754fd9f2e40dac0595cf1472
Erik Olsson <olsson.erik1993@gmail.com>
2025-10-29 13:40:44 +0100

    comments

--- app/src/main/java/com/github/mangila/app/service/EmployeeFactory.java
+++ app/src/main/java/com/github/mangila/app/service/EmployeeFactory.java
@@ -5,6 +5,12 @@ import com.github.mangila.app.model.employee.domain.EmployeeId;
 import com.github.mangila.app.model.employee.dto.CreateNewEmployeeRequest;
 import org.springframework.stereotype.Component;
 
+/**
+ * Java without a Factory Method is not Java :D
+ * <br>
+ * Spring Component Factory Method pattern, this is purely optional in this case.
+ * It's separate of concerns thinking since sometimes the mapper implementation becomes both the factory and the mapper.
+ */
 @Component
 public class EmployeeFactory {
     private final EmployeeIdGenerator employeeIdGenerator;
