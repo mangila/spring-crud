7b7041721e03becda8368d23e33577d67928a353
Erik Olsson <olsson.erik1993@gmail.com>
2025-11-01 14:18:35 +0100

    comments

--- app/src/test/java/com/github/mangila/app/service/EmployeeServiceTest.java
+++ app/src/test/java/com/github/mangila/app/service/EmployeeServiceTest.java
@@ -8,6 +8,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 
+/**
+ * Full-scale service test with integration towards a database.
+ * <br>
+ * Some might use a Mock here towards the database.
+ * If this might be developed in an Enterprise setting when behind a corporate firewall,
+ * a CI/CD server might not be able to spin up a testcontainer (might not even have Docker installed),
+ * so it won't be able to fetch from Docker public repositories.
+ * In that case you can use a Mock here and have a separate local disabled test for that.
+ * <br>
+ * But here we use a real database from Docker public repositories.
+ */
 @Import(TestcontainersConfiguration.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 class EmployeeServiceTest {
