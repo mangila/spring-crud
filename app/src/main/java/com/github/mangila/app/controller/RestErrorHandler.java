c06c3bedd494e8a0289b209534643e3973ac172e
Erik Olsson <olsson.erik1993@gmail.com>
2025-11-02 18:34:53 +0100

    error handling

--- app/src/main/java/com/github/mangila/app/controller/RestErrorHandler.java
+++ app/src/main/java/com/github/mangila/app/controller/RestErrorHandler.java
@@ -2,16 +2,24 @@ package com.github.mangila.app.controller;
 
 import com.github.mangila.app.shared.exception.EnsureException;
 import com.github.mangila.app.shared.exception.EntityNotFoundException;
+import jakarta.validation.ConstraintViolationException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
+@Slf4j
 public class RestErrorHandler {
 
+    /**
+     * Log this or not, that's the question. Might create a lot of noise in the logs.
+     * Since we often know what it's all about.
+     */
     @ExceptionHandler(EnsureException.class)
     public ProblemDetail handleEnsureException(EnsureException e) {
+        log.error("ERR", e);
         ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(
                 HttpStatus.BAD_REQUEST,
                 e.getMessage()
@@ -19,8 +27,13 @@ public class RestErrorHandler {
         return problemDetail;
     }
 
+    /**
+     * Log this or not, that's the question. Might create a lot of noise in the logs.
+     * Since we often know what it's all about.
+     */
     @ExceptionHandler(EntityNotFoundException.class)
-    public ProblemDetail handleEntityNotFoundException(Exception e) {
+    public ProblemDetail handleEntityNotFoundException(EntityNotFoundException e) {
+        log.error("ERR", e);
         ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(
                 HttpStatus.BAD_REQUEST,
                 e.getMessage()
@@ -28,20 +41,46 @@ public class RestErrorHandler {
         return problemDetail;
     }
 
+    /**
+     * Log this or not, that's the question. Might create a lot of noise in the logs.
+     * Since we often know what it's all about.
+     * Might log for some time and remove it later when the app is getting more mature.
+     */
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ProblemDetail handleConstraintViolationException(ConstraintViolationException e) {
+        log.error("ERR", e);
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(
+                HttpStatus.BAD_REQUEST,
+                e.getMessage()
+        );
+        return problemDetail;
+    }
+
+    /**
+     * This one might be a security concern to since it might expose internal details to the client.
+     * Uncontrollable runtime exceptions not thrown by the programmer.
+     */
     @ExceptionHandler(RuntimeException.class)
     public ProblemDetail handleRuntimeException(RuntimeException e) {
+        log.error("ERR", e);
         ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(
                 HttpStatus.BAD_REQUEST,
-                e.getMessage()
+                "Something went wrong. Please try again later."
         );
         return problemDetail;
     }
 
+    /**
+     * General sec practice to doesn't expose internal server errors to the client.
+     * If in an Enterprise setting within a closed network, this might be desired.
+     * But generally it's a good idea to hide internal server errors.
+     */
     @ExceptionHandler(Exception.class)
     public ProblemDetail handleException(RuntimeException e) {
+        log.error("ERR", e);
         ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(
                 HttpStatus.INTERNAL_SERVER_ERROR,
-                e.getMessage()
+                "Something went wrong. Please try again later."
         );
         return problemDetail;
     }
