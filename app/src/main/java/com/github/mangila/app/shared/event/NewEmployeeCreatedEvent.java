7dc1b4d395ede923876526f3bd1c5b11d1bcd853
Erik Olsson <olsson.erik1993@gmail.com>
2025-11-07 16:31:10 +0100

    comments, test fail stubs

--- app/src/main/java/com/github/mangila/app/shared/event/NewEmployeeCreatedEvent.java
+++ app/src/main/java/com/github/mangila/app/shared/event/NewEmployeeCreatedEvent.java
@@ -3,7 +3,7 @@ package com.github.mangila.app.shared.event;
 import com.github.mangila.app.model.employee.domain.EmployeeId;
 
 /**
- * Events often published it's good practice to keep events as small as possible.
+ * Events often published it's a good practice to keep events as small as possible.
  * With not too much data. Buffers in event-based infra can have a limit.
  */
 public record NewEmployeeCreatedEvent(EmployeeId employeeId) {
