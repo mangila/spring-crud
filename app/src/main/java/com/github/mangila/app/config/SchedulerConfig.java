7a75b0c92e6754c5f1d568f46530eccdf5899a7e
Erik Olsson <olsson.erik1993@gmail.com>
2025-11-09 19:16:45 +0100

    scheduler

--- app/src/main/java/com/github/mangila/app/config/SchedulerConfig.java
+++ app/src/main/java/com/github/mangila/app/config/SchedulerConfig.java
@@ -1,9 +1,24 @@
 package com.github.mangila.app.config;
 
+import com.github.mangila.app.scheduler.Scheduler;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Configuration
 @EnableScheduling
+@Slf4j
 public class SchedulerConfig {
+
+    @Bean
+    @ConditionalOnProperty(
+            name = "application.scheduler.enabled",
+            havingValue = "true"
+    )
+    Scheduler scheduler() {
+        log.info("Scheduler enabled");
+        return new Scheduler();
+    }
 }
--- app/src/main/java/com/github/mangila/app/scheduler/Scheduler.java
+++ app/src/main/java/com/github/mangila/app/scheduler/Scheduler.java
@@ -1,6 +1,9 @@
 package com.github.mangila.app.scheduler;
 
-import org.springframework.stereotype.Service;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import java.time.LocalDateTime;
 
 /**
  * Run some tasks, running with the Spring integrated scheduler.
@@ -8,7 +11,28 @@ import org.springframework.stereotype.Service;
  * To prevent resource exhaustion.
  * <br>
  * For minor stuff like cache eviction or stuff that belongs inside the application infra use the Spring scheduler.
+ * <br>
+ * <a href="https://spring.io/blog/2020/11/10/new-in-spring-5-3-improved-cron-expressions">Spring Scheduler stuffs</a>
  */
-@Service
+@Slf4j
 public class Scheduler {
+
+    // Run a task every 5 seconds
+    @Scheduled(fixedRateString = "${application.scheduler.fixed-rate}")
+    public void fixedRateTask() {
+        log.info("Fixed Rate Task started - {}", LocalDateTime.now());
+    }
+
+    // Run a task every 5 seconds, but only if the previous run has finished
+    @Scheduled(fixedDelayString = "${application.scheduler.fixed-delay}")
+    public void fixedDelayTask() {
+        log.info("Fixed Delay Task started - {}", LocalDateTime.now());
+    }
+
+    // Run a task at a specific time
+    @Scheduled(cron = "${application.scheduler.cron}")
+    public void cronTask() {
+        log.info("Cron Task started - {}", LocalDateTime.now());
+    }
+
 }
