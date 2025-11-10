398da42e0b4d3292025b33d95acc89edc76c825d
Erik Olsson <olsson.erik1993@gmail.com>
2025-11-10 15:19:20 +0100

    jspecify nullmark repository package

--- app/src/main/java/com/github/mangila/app/repository/package-info.java
+++ app/src/main/java/com/github/mangila/app/repository/package-info.java
@@ -3,4 +3,7 @@
 // https://vladmihalcea.com/spring-data-findall-anti-pattern/
 // let's do as Vlad says in this project, and I agree.
 // It's easy to get lost in SQL translation with the default jpa repository
+@NullMarked
 package com.github.mangila.app.repository;
+
+import org.jspecify.annotations.NullMarked;
