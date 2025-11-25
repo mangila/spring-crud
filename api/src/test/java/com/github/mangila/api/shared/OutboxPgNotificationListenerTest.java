229117bc333d9ee10953e8640ed61c858a3dbd47
Erik Olsson <olsson.erik1993@gmail.com>
2025-11-25 03:15:05 +0100

    server side events

---
+++ api/src/test/java/com/github/mangila/api/shared/OutboxPgNotificationListenerTest.java
@@ -0,0 +1,13 @@
+package com.github.mangila.api.shared;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class OutboxPgNotificationListenerTest {
+
+    @Test
+    void run() {
+        assertThat(1 + 1).isEqualTo(3);
+    }
+}
