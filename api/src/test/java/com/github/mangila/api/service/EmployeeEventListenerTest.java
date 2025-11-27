1dfb6904288ddfcd33d7386dee0f6e7adffe72c8
Erik Olsson <olsson.erik1993@gmail.com>
2025-11-27 16:01:51 +0100

    test listener

--- api/src/test/java/com/github/mangila/api/service/EmployeeEventListenerTest.java
+++ api/src/test/java/com/github/mangila/api/service/EmployeeEventListenerTest.java
@@ -2,27 +2,36 @@ package com.github.mangila.api.service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.mangila.api.EmployeeTestFactory;
+import com.github.mangila.api.OutboxTestFactory;
 import com.github.mangila.api.PostgresTestContainerConfiguration;
 import com.github.mangila.api.model.employee.dto.EmployeeEventDto;
 import com.github.mangila.api.model.employee.event.CreateNewEmployeeEvent;
-import com.github.mangila.api.model.outbox.OutboxEvent;
+import com.github.mangila.api.model.outbox.OutboxEntity;
+import com.github.mangila.api.model.outbox.OutboxEventStatus;
+import com.github.mangila.api.model.outbox.OutboxNextSequenceEntity;
+import com.github.mangila.api.model.outbox.OutboxProcessedSequenceEntity;
 import com.github.mangila.api.repository.OutboxJpaRepository;
+import com.github.mangila.api.repository.OutboxNextSequenceJpaRepository;
+import com.github.mangila.api.repository.OutboxProcessedSequenceJpaRepository;
 import com.github.mangila.api.shared.SpringEventPublisher;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.system.OutputCaptureExtension;
 import org.springframework.context.annotation.Import;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.bean.override.mockito.MockitoSpyBean;
 import org.springframework.transaction.support.TransactionTemplate;
 
 import java.io.IOException;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.awaitility.Awaitility.await;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.inOrder;
-import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 /**
  * Load the whole context and publish events
@@ -63,25 +72,48 @@ class EmployeeEventListenerTest {
     private OutboxJpaRepository outboxJpaRepository;
 
     @MockitoSpyBean
+    private OutboxNextSequenceJpaRepository outboxNextSequenceRepository;
+
+    @MockitoSpyBean
+    private OutboxProcessedSequenceJpaRepository outboxProcessedSequenceJpaRepository;
+
+    @MockitoSpyBean
     private OutboxEventMapper outboxEventMapper;
+
+    @AfterEach
+    void afterEach() {
+
+    }
+
+    @BeforeEach
+    void beforeEach() {
+    }
 
     @Test
     void listen() throws IOException {
         EmployeeEventDto dto = EmployeeTestFactory.createEmployeeEventDto(objectMapper);
+        OutboxNextSequenceEntity next = OutboxTestFactory.createOutboxNextSequenceEntity(dto.employeeId());
+        next.setSequence(3);
+        outboxNextSequenceRepository.persist(next);
+        OutboxProcessedSequenceEntity processed = OutboxTestFactory.createOutboxProcessedSequenceEntity(dto.employeeId());
+        processed.setSequence(3);
+        outboxProcessedSequenceJpaRepository.persist(processed);
         transactionTemplate.executeWithoutResult(txStatus -> {
             publisher.publish(
                     dto.employeeId(),
                     new CreateNewEmployeeEvent(dto)
             );
         });
-        var inOrder = inOrder(
-                listener,
-                eventHandler,
-                outboxFactory,
-                outboxJpaRepository,
-                outboxEventMapper);
-        inOrder.verify(listener, times(1)).listen(any(OutboxEvent.class));
-        assertThat(1 + 1).isEqualTo(3);
+
+        await().untilAsserted(() -> {
+            verify(listener).listen(any());
+            verify(eventHandler).handle(any());
+            OutboxEntity entity = outboxJpaRepository.findAllByAggregateId(dto.employeeId(), Pageable.unpaged())
+                    .getContent()
+                    .getFirst();
+            assertThat(entity.getStatus())
+                    .isEqualTo(OutboxEventStatus.PUBLISHED);
+        });
     }
 
 }
