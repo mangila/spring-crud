6ac030a8a31d730c1d55163a8be3c904ea9d9103
Erik Olsson <olsson.erik1993@gmail.com>
2025-12-04 23:07:58 +0100

    tf

--- infrastructure/local/outbound.tf
+++ infrastructure/local/outbound.tf
@@ -1,11 +1,3 @@
 output "ansible_inventory_ini_file" {
   value = local_file.ansible_inventory_ini_file
 }
-
-output "generate_ssh_key_pair" {
-  value = terraform_data.generate_ssh_key_pair
-}
-
-output "generate_ssh_key_pair_command" {
-  value = terraform_data.generate_ssh_key_pair.input
-}
