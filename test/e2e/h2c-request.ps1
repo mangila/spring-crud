13de091dbfbcf8feb3e3a6c4d9408e82ad420111
Erik Olsson <olsson.erik1993@gmail.com>
2025-12-04 18:16:10 +0100

    bash scrits

--- test/e2e/h2c-request.ps1
+++
@@ -1,5 +0,0 @@
-# alias httpc is my local systems curl binary with http2 features
-# curl is reserved by Windows
-$headers = httpc -I --http2 http://localhost:8080/api/v1/employees
-
-Write-Host $headers
