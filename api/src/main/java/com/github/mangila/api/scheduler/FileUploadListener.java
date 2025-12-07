aba5dd547de2a003abcfcb21d9faa4b2ab5d808a
Erik Olsson <olsson.erik1993@gmail.com>
2025-12-07 13:05:46 +0100

    file uplaods

--- api/src/main/java/com/github/mangila/api/scheduler/FileUploadListener.java
+++ api/src/main/java/com/github/mangila/api/scheduler/FileUploadListener.java
@@ -1,7 +1,33 @@
 package com.github.mangila.api.scheduler;
 
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import lombok.Getter;
 import org.springframework.stereotype.Component;
 
+import java.util.concurrent.Callable;
+
 @Component
-public class FileUploadListener {
+public class FileUploadListener implements Callable<ObjectNode> {
+
+    @Getter
+    private volatile boolean running = false;
+    @Getter
+    private volatile boolean shutdown = false;
+
+    public void start() {
+        running = true;
+    }
+
+    public void stop() {
+        running = false;
+    }
+
+    public void shutdown() {
+        shutdown = true;
+    }
+
+    @Override
+    public ObjectNode call() throws Exception {
+        return null;
+    }
 }
--- api/src/main/java/com/github/mangila/api/scheduler/FileUploadWatcher.java
+++ api/src/main/java/com/github/mangila/api/scheduler/FileUploadWatcher.java
@@ -1,23 +1,49 @@
 package com.github.mangila.api.scheduler;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.github.mangila.api.shared.ApplicationTaskExecutor;
 import org.springframework.context.SmartLifecycle;
 import org.springframework.stereotype.Component;
+
+import java.util.concurrent.CompletableFuture;
 
 @Component
 public class FileUploadWatcher implements SmartLifecycle {
 
+    private final FileUploadListener listener;
+    private final ApplicationTaskExecutor applicationTaskExecutor;
+    private final ObjectMapper objectMapper;
+    private CompletableFuture<ObjectNode> listenEventLoop;
+
+    public FileUploadWatcher(FileUploadListener listener,
+                             ApplicationTaskExecutor applicationTaskExecutor,
+                             ObjectMapper objectMapper) {
+        this.listener = listener;
+        this.applicationTaskExecutor = applicationTaskExecutor;
+        this.objectMapper = objectMapper;
+    }
+
     @Override
     public void start() {
-
+        listener.start();
+        var attributes = objectMapper.createObjectNode();
+        attributes.put("executedBy", FileUploadWatcher.class.getSimpleName());
+        this.listenEventLoop = applicationTaskExecutor.submitCompletable(
+                listener,
+                "file-upload-listener",
+                attributes
+        );
     }
 
     @Override
     public void stop() {
-
+        listener.shutdown();
+        listenEventLoop.cancel(true);
     }
 
     @Override
     public boolean isRunning() {
-        return false;
+        return listener.isRunning();
     }
 }
